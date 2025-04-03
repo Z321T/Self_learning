ae6bcce1d29b76d4c0ef5d3ed23dcf8a386b61f8
Nacht ohne sterne <131273673+Z321T@users.noreply.github.com>
2025-04-03 19:41:09 +0800

    0403

--- 考研数据结构/排序/main.cpp
+++ 考研数据结构/排序/main.cpp
@@ -97,6 +97,52 @@ void QuickSort(int arr[], int low, int high)
     }
 }
 
+//选择排序
+//简单选择排序
+void SelectSort(int arr[], int n)
+{
+    for (int i = 0; i < n - 1; i++)
+    {
+        int minIndex = i;
+        for (int j = i + 1; j < n; j++)
+        {
+            if (arr[j] < arr[minIndex])
+                minIndex = j;
+        }
+        swap(arr[i], arr[minIndex]);
+    }
+}
+//堆排序
+void Heapify(int arr[], int n, int i)
+{
+    int largest = i;
+    int left = 2 * i + 1;
+    int right = 2 * i + 2;
+
+    if (left < n && arr[left] > arr[largest])
+        largest = left;
+
+    if (right < n && arr[right] > arr[largest])
+        largest = right;
+
+    if (largest != i)
+    {
+        swap(arr[i], arr[largest]);
+        Heapify(arr, n, largest);
+    }
+}
+void HeapSort(int arr[], int n)
+{
+    for (int i = n / 2 - 1; i >= 0; i--)
+        Heapify(arr, n, i);
+
+    for (int i = n - 1; i >= 0; i--)
+    {
+        swap(arr[0], arr[i]);
+        Heapify(arr, i, 0);
+    }
+}
+
 
 
 int main()
@@ -130,5 +176,17 @@ int main()
     for (int i = 0; i < n; i++)
         cout << arr[i] << " ";
     cout << endl;
+    //选择排序
+    //简单选择排序
+    SelectSort(arr, n);
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+    cout << endl;
+    //堆排序
+    HeapSort(arr, n);
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+    cout << endl;
+
     return 0;
 }
