224fc8b883f86671102b214753a1c91ec654b4f2
Nacht ohne sterne <fzc19942496944@outlook.com>
2025-04-11 22:50:47 +0800

    0411

--- 算法学习/luogu/[入门1]顺序结构/P5707/main.cpp
+++ 算法学习/luogu/[入门1]顺序结构/P5707/main.cpp
@@ -3,6 +3,58 @@ using namespace std;
 
 int main()
 {
+    int s,v;
+    cin>>s>>v;
+    //计算时间花费，向上取整
+    int t = s/v;//路程上花费的时间--分钟
+    if (s%v != 0)
+        t++;
+    t+=10;//垃圾分类的时间
+
+    int ans_h, ans_m;
+    //计算最晚出发时间
+    int cos_h = t/60;
+    int cos_m = t%60;
+
+    if (cos_h < 8) {
+        //当天之内出发
+        if (cos_m == 0) {
+            ans_h = 8 - cos_h;
+            ans_m = 0;
+        }
+        else {
+            ans_h = 8 - cos_h - 1;
+            ans_m = 60 - cos_m;
+        }
+    }
+    if (cos_h == 8) {
+        if (cos_m == 0) {
+            ans_h = 8 - cos_h;
+            ans_m = 0;
+        }
+        else {
+            ans_h = 23;
+            ans_m = 60 - cos_m;
+        }
+    }
+    if (8 < cos_h && cos_h < 24) {
+        //提前一天之内出发
+        if (cos_m == 0) {
+            ans_h = 24 - cos_h + 8;
+        }
+        else {
+            ans_h = 24 - cos_h - 1 + 8;
+            ans_m = 60 - cos_m;
+        }
+    }
+
+    //输出符合要求的格式化结果
+    if (ans_h/10 == 0) {
+        cout << "0" << ans_h << ":" << ans_m;
+    }
+    else {
+        cout<<ans_h<<":"<<ans_m;
+    }
 
     return 0;
 }
